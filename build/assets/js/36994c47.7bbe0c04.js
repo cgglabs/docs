1fb48bb554200e732b028bf41a8ad51929af242e
cggonzal <gonzalezcgg1@gmail.com>
2025-04-12 19:43:46 -0700

    add production files and modify gitignore

---
+++ build/assets/js/36994c47.7bbe0c04.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkcgg_labs_docs=self.webpackChunkcgg_labs_docs||[]).push([[858],{5516:s=>{s.exports=JSON.parse('{"name":"docusaurus-plugin-content-blog","id":"default"}')}}]);
---
+++ build/assets/js/acecf23e.df10c67d.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkcgg_labs_docs=self.webpackChunkcgg_labs_docs||[]).push([[903],{1912:s=>{s.exports=JSON.parse('{"blogBasePath":"/blog","blogTitle":"Blog","authorsListPath":"/blog/authors"}')}}]);
